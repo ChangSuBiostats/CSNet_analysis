1c93da54585b7e8ae4b348ecfbdc87cd329cd13d
Chang Su <c.su@yale.edu>
2023-06-05 22:39:52 -0400

    update heatmaps: options to disable legends; use color-blind friendly panels

--- code/simulation/visualization_helper.R
+++ code/simulation/visualization_helper.R
@@ -5,7 +5,9 @@
 
 plot_heatmap <- function(cor_m, sub_cl, title,
                          legend = T,
-                         annotation_legend = T){
+                         annotation_legend = T,
+			 annotation_names = T,
+			 silent = F){
   require(pheatmap)
   require(RColorBrewer)
   # color for heatmap
@@ -24,15 +26,16 @@ plot_heatmap <- function(cor_m, sub_cl, title,
   if(is.null(rownames(cor_m))){
       rownames(cor_m) <- colnames(cor_m) <- paste0('Gene_', 1:p)
   }
-
+  
+  # http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/#:~:text=The%20default%20colors%20in%20ggplot2,not%20friendly%20for%20colorblind%20viewers.
   my_colors = list(
-    cluster = c('1'='#65C1E8',
-                '2'='#D85B63',
-                '3'='#D680AD',
-                '4'='#5C5C5C',
-                '5'='#C0BA80',
-                '6'='#FDC47D',
-                '7'='#EA3B46')[1:length(unique(labels))]
+    cluster = c('1'='#E69F00',
+                '2'='#56B4E9',
+                '3'='#009E73',
+                '4'='#F0E442',
+                '5'='#0072B2',
+                '6'='#D55E00',
+                '7'='#CC79A7')[1:length(unique(labels))]
   )
   gene_cl <- data.frame(cluster = labels %>% as.character)
   rownames(gene_cl) <- rownames(cor_m)
@@ -49,6 +52,9 @@ plot_heatmap <- function(cor_m, sub_cl, title,
                     annotation_colors = my_colors,
                     annotation_legend = annotation_legend,
                     legend = legend,
-                    cellwidth = 2, cellheight = 2)
+		    annotation_names_row = annotation_names, annotation_names_col = annotation_names,
+                    cellwidth = 2, cellheight = 2,
+		    silent = silent)
+  h$gtable$grobs[[1]]$gp <- gpar(fontsize = 25, fontfamily = 'sans')
   return(h)
 }
